46dd7d85aec11c08681c1ad6bddd51b2aa91a269
Kuba <kjuchnowski@gmail.com>
2022-10-12 17:30:25 +0200

    minor changes.

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'IndicatorAPI.com wrapper'
 LONG_DESCRIPTION = 'A package that allows developers to use indicatorapi quickly.'
-
+project_urls = {'GitHub': 'https://github.com/dreistein33/pyIndicatorAPI'}
 # Setting up
 setup(
     name="pyIndicatorAPI",
     version=VERSION,
     author="dreistein33",
-    author_email="kjuchnowski@wp.pl",
+    author_email="kjuchnowski@gmail.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['datetime', 'os', 'requests'],
+    project_urls=project_urls,
+    install_requires=['datetime', 'requests'],
     keywords=['python', 'indicatorapi', 'api', 'crypto'],
     classifiers=[
         "Development Status :: 3 - Alpha",
